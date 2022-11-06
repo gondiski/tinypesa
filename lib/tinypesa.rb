a1e6592dcb08861e5871b141b6aed997f4e98719
Enoch Otieno <enochotieno@rocketmail.com>
2022-11-06 21:05:36 +0300

    Add the tinypesa header authorization and request parameters

--- lib/tinypesa.rb
+++ lib/tinypesa.rb
@@ -1,12 +1,21 @@
 require "tinypesa/version"
 require "httparty"
 require "dotenv/load"
+
 module Tinypesa
   class Error < StandardError; end
   # Your code goes here...
   class Pay
     def self.stk(amount,number)
-      url = "http://www.tinypesa.com/api/v1/express/initialize"
+      response = HTTParty.post("http://www.tinypesa.com/api/v1/express/initialize",
+                              headers:{
+                              'authorization'=> "Bearer #{ENV[TINYPESA_KEY]}",
+                              'content_type'=>'application/json'},
+                              query:{
+                                amount: amount,
+                                number: number}
+                              )
+      
       return "Transaction made"
     end
   end
