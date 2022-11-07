4d742589bf2be3c655a3ba9fb4af7f0624b8e918
Enoch Otieno <enochotieno@rocketmail.com>
2022-11-07 22:30:23 +0300

    Revert "Revert "Add changelog and write methods for merchant and status""
    This reverts commit 2eacdd2a3195574886f17e44dd926b3e0fb24705.

--- lib/tinypesa.rb
+++ lib/tinypesa.rb
@@ -18,5 +18,35 @@ module Tinypesa
       
       return "Transaction made"
     end
+
+    def self.status(account_no)
+      response = HTTParty.get("http://www.tinypesa.com/api/v1/express/get_status",
+                              headers:{
+                              'authorization'=> "Bearer #{ENV[TINYPESA_KEY]}",
+                              'content_type'=>'application/json'},
+                              query:{
+                                account_no: account_no}
+                              )
+      return response.message
+    end
+  end
+  class Merchant
+    def self.create(name,description,link_mode,short_code,account_number,bank,bank_acc_no,call_back_url)
+      response = HTTParty.post("http://www.tinypesa.com/api/v1/express/create_link",
+                              headers:{
+                              'authorization'=> "Bearer #{ENV[TINYPESA_KEY]}",
+                              'content_type'=>'application/json'},
+                              query:{
+                                name: name,
+                                description: description,
+                                link_mode: link_mode,
+                                short_code: short_code,
+                                account_number: account_number,
+                                bank: bank
+                                bank_acc_no: bank_acc_no,
+                                call_back_url: call_back_url}
+                              )
+      return response
+    end
   end
 end
